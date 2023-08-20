1c7e13fbbb53e7691e8376f6c326c1d12e2ee1ac
Marcelo Rodrigues <seu-email@example.com>
2023-08-20 11:36:44 -0300

    aplicando SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,10 @@
 import Image from "next/image"
-import { styled } from "../styles"
 import { HomeContainer, Product } from "../styles/pages/home"
-// import { Stripe } from '@stripe/stripe-js';
 
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Stripe from "stripe"
 
 import { useKeenSlider } from 'keen-slider/react'
-
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
-import camiseta4 from '../assets/camisetas/4.png'
 
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from "../lib/stripe"
@@ -60,7 +53,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps : GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -76,11 +69,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
     }
   })
 
-  console.log(response.data)
-  console.log('response.data')
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2, //2 horas
   }
 }
